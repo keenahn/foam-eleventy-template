115fbd7c6aac4bd1703afbfb472f9770626f1754
zomars <zomars@me.com>
2020-08-26 09:38:06 -0600

    Fixes nested wiki-links
    # Conflicts:
    #	.eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,47 @@
 module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy(".htaccess");
+  eleventyConfig.addTransform("wiki-links", function (content, outputPath) {
+    if (outputPath.endsWith(".html")) {
+      // We remove outer brackets from links
+      let output = content.replace(/(\[+(\<a(.*?)\<\/a\>)\]+)/g, "$2");
+      return output;
+    }
+    return content;
+  });
+
+  let markdownIt = require("markdown-it");
+  let markdownItReplaceLink = require("markdown-it-replace-link");
+  let markdownItOptions = {
+    html: true,
+    replaceLink: function (link, env) {
+      const isRelativePattern = /^(?!http|\/).*/;
+      const lastSegmentPattern = /[^\/]+(?=\/$|$)/i;
+      const isRelative = isRelativePattern.test(link);
+
+      if (isRelative) {
+        return env.page.url.replace(lastSegmentPattern, link);
+      }
+
+      return link;
+    },
+  };
+  let markdownLib = markdownIt(markdownItOptions).use(markdownItReplaceLink);
+  eleventyConfig.setLibrary("md", markdownLib);
 
   return {
     dir: {
       input: ".",
       output: "_site",
       includes: "_includes",
-      layouts: "_layouts"
+      layouts: "_layouts",
     },
     templateFormats: [
+      //
       "js",
       "md",
       "html",
-      "liquid"
+      "liquid",
     ],
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
   };
 };
