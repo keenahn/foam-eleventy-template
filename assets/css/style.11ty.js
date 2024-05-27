a9997695e2036b61efb945ec90080a2eede088ff
Keenahn Tiberius Jung <keenahn@users.noreply.github.com>
2024-05-27 16:32:43 -0400

    fix: replace node-sass-promise with sass

--- assets/css/style.11ty.js
+++ assets/css/style.11ty.js
@@ -1,8 +1,9 @@
 const path = require('path')
-const sass = require('node-sass-promise')
+const sass = require('sass');
+
 const CleanCSS = require('clean-css')
 
-const inputFile = path.join(__dirname, '../../_includes/scss/main.scss')
+const inputFile = path.join(__dirname, '../../_includes/scss/main.sass')
 const outputFile = path.join(__dirname, '../../assets/css/style.css')
 
 module.exports = class {
@@ -14,10 +15,9 @@ module.exports = class {
     }
   }
 
-  async render() {
-    const { css } = await sass.render({ file: inputFile })
-    const output = new CleanCSS({}).minify(css.toString()).styles
-
+  render() {
+    const { css } = sass.compile(inputFile)
+    const output = new CleanCSS({}).minify(css).styles
     return output
   }
 }
